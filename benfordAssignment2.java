bef35f8d6d8d89284ab428cd4d62b5b7e48b5615
chloechak <chloechak0@gmail.com>
2021-05-05 17:25:13 -0400

    first commit

---
+++ benfordAssignment2.java
@@ -0,0 +1,16 @@
+/**
+ * Date:
+ * Author:
+ * Teacher:
+ * Description: 
+ */
+class benfordAssignment2 {
+    public static void main(String[] args) {
+        // step 1: ask user if they would like to read file or check for fraud
+        // Step 2 (option a): to read file, load the sales data from csv
+        // step 2 (option b): program checks for fraud through the benfords law (create graph)
+        // continuation of step 2 option b: the legend of the graph ( ex. 1: 28% 2: 34%) will be transferred to a csv file called results.csv
+        // hello
+        // hello 
+    }
+}
